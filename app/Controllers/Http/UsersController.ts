60304a70b4f5765fabd23a16177531a4c5b9b7fe
Paulo Matos <paulomatos4554@gmail.com>
2021-12-16 09:31:59 -0300

    add query builder paramns in crud operations

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,23 +1,32 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
+import { Request } from '@adonisjs/core/build/standalone'
 
 export default class UsersController {
   public async index({}: HttpContextContract) {
     return 'all users'
   }
 
-  public async store({}: HttpContextContract) {
-    return ' create user'
+  public async store({ request }: HttpContextContract) {
+    const data = request.only(['name', 'email', 'password'])
+    const user = await User.create(data)
+    return user
   }
 
-  public async show({}: HttpContextContract) {
-    return ' get user by id'
+  public async show({ params }: HttpContextContract) {
+    const user = await User.find(params.id)
+    return user
   }
 
-  public async update({}: HttpContextContract) {
-    return ' change user '
+  public async update({ request, params }: HttpContextContract) {
+    const data = request.only(['name', 'email', 'password'])
+    const user = await User.findOrFail(params.id)
+    user.merge(data)
+    await user.save()
   }
 
-  public async destroy({}: HttpContextContract) {
-    ' delete user'
+  public async destroy({ params }: HttpContextContract) {
+    const user = await User.findOrFail(params.id)
+    await user.delete()
   }
 }
